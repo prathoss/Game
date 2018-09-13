fbb9a5b03d1b9d633e1f45f3a37232b6c40977fa
Michal Bradac <m.bradac.michal@gmail.com>
2018-09-13 11:54:55 +0200

    showing enemy health

--- HackAndSlash/Assets/Scripts/Enemy.cs
+++ HackAndSlash/Assets/Scripts/Enemy.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
     private int _health = 100;
+    public Text healthText;
 
-    public int Health { get { return _health; } }
+    public string Health { get { return "Health: " + _health.ToString(); } }
 
     public void TakeDamage(int damage)
     {
@@ -12,6 +14,11 @@ public class Enemy : MonoBehaviour {
         if(_health < 0)
         {
             Object.Destroy(gameObject);
+            healthText.text = string.Empty;
+        }
+        else
+        {
+            healthText.text = Health;
         }
     }
 }
