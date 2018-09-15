6239899e3b3aabecc4f1b2813c9ce917773dae3f
seb hruby <sebastianhruby@gmail.com>
2018-09-15 22:01:38 +0200

    Player Motion update

---
+++ HackAndSlash/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+    Camera cam;
+    PlayerMotor motor;
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+	}
+}
