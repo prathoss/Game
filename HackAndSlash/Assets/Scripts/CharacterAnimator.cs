cd7959bc6027b0e81efae7bdcbfb0c1721339643
seb hruby <sebastianhruby@gmail.com>
2018-09-15 20:27:36 +0200

    Character Animation Script

---
+++ HackAndSlash/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    const float locomotionAnimationSmoothTime = 0.1f;
+    NavMeshAgent agent;
+    protected Animator animator;
+
+    public AnimatorOverrideController overrideController;
+
+	// Use this for initialization
+	protected virtual void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+
+        if (overrideController == null)
+        {
+            overrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        }        
+        animator.runtimeAnimatorController = overrideController;
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+	}
+}
