205d863f4727665f3d025bba4e1f7f1745436d8f
seb hruby <sebastianhruby@gmail.com>
2018-09-15 17:11:40 +0200

    Player manager + enemy detection

---
+++ HackAndSlash/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    public float lookRadius = 3f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
